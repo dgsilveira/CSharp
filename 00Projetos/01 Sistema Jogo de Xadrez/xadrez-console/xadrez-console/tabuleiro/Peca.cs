576f6d5f4925cce0b687f31d38fd5861647dcb46
dgsilveira <dgsilveira@sga.pucminas.br>
2022-09-06 11:07:08 -0300

    Implementando as restricoes de movimento

--- 00Projetos/01 Sistema Jogo de Xadrez/xadrez-console/xadrez-console/tabuleiro/Peca.cs
+++ 00Projetos/01 Sistema Jogo de Xadrez/xadrez-console/xadrez-console/tabuleiro/Peca.cs
@@ -2,10 +2,12 @@
 {
     abstract class Peca
     {
+
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
+
         public Peca(Tabuleiro tab, Cor cor)
         {
             this.posicao = null;
@@ -13,9 +15,31 @@
             this.cor = cor;
             this.qteMovimentos = 0;
         }
+
         public void incrementarQteMovimentos()
         {
             qteMovimentos++;
+        }
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
         }
 
         public abstract bool[,] movimentosPossiveis();
