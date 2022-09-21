2bf92d57d7fc39cecffd65b2014ae80e447aa87e
dgsilveira <dgsilveira@sga.pucminas.br>
2022-09-21 08:37:23 -0300

    FilmeAPI
    Repositório público

---
+++ 01 Diversos/API/00 API Rest com .NET - Models Filme/FilmeAPI/FilmeAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmeAPI.Data.Dtos
+{
+    public class UpdateFilmeDto
+    {
+        [Required(ErrorMessage = "O Campo título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Campo diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero deve conter no mínimo 3 e máximo 30 caracteres", MinimumLength = 3)]
+        public string Genero { get; set; }
+        [Range(5, 200, ErrorMessage = "A duração deve ter no mínimo 1 e no máixmo 200 minutos")]
+        public int Duracao { get; set; }
+    }
+}
---
+++ 01 Diversos/API/00 API Rest com .NET - Models Filme/FilmeAPI/FilmeAPI/Data/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmeAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
